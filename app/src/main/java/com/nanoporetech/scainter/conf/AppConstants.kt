9922cf424f8f0a2cabfdb4361fe87719a9accf84
Jules Konan <jules.konan@nanoporetech.com>
2026-04-08 17:32:47 +0100

    add LoginScreen

---
+++ app/src/main/java/com/nanoporetech/scainter/conf/AppConstants.kt
@@ -0,0 +1,11 @@
+package com.nanoporetech.scainter.conf
+
+import androidx.compose.ui.graphics.Color
+
+object AppConstants {
+    val lightGreen = Color(
+        red = 231f / 255f,
+        green = 237f / 255f,
+        blue = 239f / 255f
+    )
+}
--- app/src/main/java/com/nanoporetech/scainter/ui/LoginScreen.kt
+++ app/src/main/java/com/nanoporetech/scainter/ui/LoginScreen.kt
@@ -1,9 +1,9 @@
 package com.nanoporetech.scainter.ui
 
-import android.R.attr.fontWeight
-import android.R.attr.text
+import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,40 +11,101 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nanoporetech.scainter.R
-import com.nanoporetech.scainter.ui.theme.ExtendedColorScheme
+import com.nanoporetech.scainter.conf.AppConstants
 import com.nanoporetech.scainter.ui.theme.ScaInterAppTheme
 import com.nanoporetech.scainter.ui.theme.ScaInterTheme
 
+private const val TAG = "LoginScreen"
 @Composable
 fun LoginScreen(
     modifier: Modifier = Modifier
 ) {
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
+    val largePadding = dimensionResource(R.dimen.padding_large)
 
-    Column(modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier) {
+
         // HEADER SECTION
         HeaderAndLogo(
-
+            modifier = Modifier
+                .fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.height(mediumPadding))
+        Spacer(modifier = Modifier.height(largePadding))
 
         // WELCOME MESSAGE SECTION
+        WelcomeMessage(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(largePadding))
+
+        // CREDENTIALS SECTION
+        CredentialsSection(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(largePadding))
+
+        // FORGOTTEN PASSWORD SECTION
+        ForgottenPasswordSection(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(largePadding))
+
+        // LOGIN BUTTON
+        Button(
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(
+                containerColor = ScaInterTheme.extendedColors.mainGreen.color,
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.login)
+            )
+        }
     }
 }
 
@@ -57,8 +118,7 @@ fun HeaderAndLogo(
     Row(horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
-            .fillMaxWidth()
-            .border(1.dp, color = Color.Red)
+            //.border(1.dp, color = Color.Red)
     ) {
         Image(
             painter = painterResource(R.drawable.sca_logo_no_title),
@@ -78,14 +138,154 @@ fun HeaderAndLogo(
     }
 }
 
+@Composable
+fun CredentialsSection(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
+        modifier = modifier) {
+        val focusManager = LocalFocusManager.current
 
-@Preview(showBackground = true)
+        OutlinedTextField(
+            value = "",
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(stringResource(R.string.username_label))
+            },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.surface,
+                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                disabledContainerColor = MaterialTheme.colorScheme.surface,
+                focusedIndicatorColor = MaterialTheme.colorScheme.surface,
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.surface,
+                disabledIndicatorColor =  MaterialTheme.colorScheme.surface,
+            ),
+            onValueChange = {},
+            isError = false,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Next
+            ),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            ),
+            visualTransformation = VisualTransformation.None,
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(dimensionResource(R.dimen.elevation_small))
+        )
+
+        OutlinedTextField(
+            value = "",
+            singleLine = true,
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = null
+                )
+            },
+            label = {
+                Text(
+                    text = stringResource(R.string.password_label)
+                )
+            },
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.surface,
+                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                disabledContainerColor = MaterialTheme.colorScheme.surface,
+                focusedIndicatorColor = MaterialTheme.colorScheme.surface,
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.surface,
+                disabledIndicatorColor =  MaterialTheme.colorScheme.surface,
+            ),
+            onValueChange = {},
+            isError = false,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {}
+            ),
+            visualTransformation = VisualTransformation.None,
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(dimensionResource(R.dimen.elevation_small))
+        )
+    }
+}
+
+@Composable
+fun WelcomeMessage(
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = stringResource(R.string.welcome_message),
+        color = ScaInterTheme.extendedColors.mainGreen.color,
+        style = MaterialTheme.typography.displayMedium,
+        fontWeight = FontWeight.SemiBold,
+        textAlign = TextAlign.Center,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun ForgottenPasswordSection(
+    modifier: Modifier = Modifier
+) {
+    Row(horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+        ) {
+            Switch(
+                checked = false,
+                onCheckedChange = {},
+            )
+
+            Text(
+                text = stringResource(R.string.remember_me),
+                color = MaterialTheme.colorScheme.primary,
+                fontSize = 16.sp
+            )
+        }
+
+        Text(
+            text = stringResource(R.string.password_forgotten),
+            fontSize = 16.sp,
+            color = MaterialTheme.colorScheme.primary,
+            textDecoration = TextDecoration.Underline,
+            modifier = Modifier
+                .clickable(
+                    //interactionSource = remember { MutableInteractionSource() },
+                    //indication = LocalIndication.current,
+                    onClick = {
+                        Log.d(TAG, "connecting")
+                    }
+                )
+        )
+    }
+}
+
+
+@Preview(
+    locale = "fr-rCI",
+    showBackground = true)
 @Composable
 fun LoginScreenPreview() {
     ScaInterAppTheme() {
         LoginScreen(
             modifier = Modifier
                 .fillMaxSize()
+                .background(AppConstants.lightGreen)
+                .padding(dimensionResource(R.dimen.padding_medium))
         )
     }
 }
