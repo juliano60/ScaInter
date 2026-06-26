f326e88b1dfd776797314f2c4c4b0b793d7cd191
Jules Konan <jules.konan@nanoporetech.com>
2026-06-26 10:26:14 +0100

    Tweak healthcare homepage

--- app/src/main/java/com/nanoporetech/scainter/ui/menu/HealthCareScreen.kt
+++ app/src/main/java/com/nanoporetech/scainter/ui/menu/HealthCareScreen.kt
@@ -1,26 +1,21 @@
 package com.nanoporetech.scainter.ui.menu
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.nanoporetech.scainter.R
 import com.nanoporetech.scainter.data.DataSource
 import com.nanoporetech.scainter.model.Provider
 import com.nanoporetech.scainter.model.displayedName
 import com.nanoporetech.scainter.ui.components.MainHeader
-import com.nanoporetech.scainter.ui.components.SubHeader
 import com.nanoporetech.scainter.ui.consultation.ConsultationCard
 import com.nanoporetech.scainter.ui.examination.ExaminationCard
 import com.nanoporetech.scainter.ui.hospitalisation.HospitalisationCard
@@ -38,59 +33,45 @@ fun HealthCareScreen(
     onViewExaminations: () -> Unit = {},
     onViewHospitalisations: () -> Unit = {},
 ) {
-    Box(
+    val spacingLarge = dimensionResource(R.dimen.vertical_spacing_xlarge)
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(spacingLarge),
         modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
     ) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.vertical_spacing_small)),
+        // MAIN HEADER SECTION
+        MainHeader(
+            title = provider.displayedName,
             modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(rememberScrollState())
-        ) {
-            // MAIN HEADER SECTION
-            MainHeader(
-                title = provider.displayedName,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(all = dimensionResource(R.dimen.padding_small))
-            )
+                .fillMaxWidth()
+                .padding(all = dimensionResource(R.dimen.padding_small))
+        )
 
-            // SUB HEADER SECTION
-            SubHeader(
-                title = stringResource(R.string.dashboard),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = dimensionResource((R.dimen.padding_small)))
-            )
+        // CONSULTATION MENU
+        ConsultationCard(
+            onNewConsultation = onNewConsultation,
+            onViewConsultations = onViewConsultations,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
 
-            // CONSULTATION MENU
-            ConsultationCard(
-                onNewConsultation = onNewConsultation,
-                onViewConsultations = onViewConsultations,
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
+        // EXAMINATION MENU
+        ExaminationCard(
+            onNewExamination = onNewExamination,
+            onViewExamination = onViewExaminations,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
 
-            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.vertical_spacing_medium)))
-
-            // EXAMINATION MENU
-            ExaminationCard(
-                onNewExamination = onNewExamination,
-                onViewExamination = onViewExaminations,
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-
-            Spacer(modifier = Modifier.height(dimensionResource(R.dimen.vertical_spacing_medium)))
-
-            // HOSPITALISATION MENU
-            HospitalisationCard(
-                onNewHospitalisation = onNewHospitalisation,
-                onViewHospitalisation = onViewHospitalisations,
-                modifier = Modifier
-                    .fillMaxWidth()
-            )
-        }
+        // HOSPITALISATION MENU
+        HospitalisationCard(
+            onNewHospitalisation = onNewHospitalisation,
+            onViewHospitalisation = onViewHospitalisations,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
     }
 }
 
